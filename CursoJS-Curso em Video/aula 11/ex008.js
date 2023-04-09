d43acf0b85bcf129e6fc530187887837111d594d
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-04-09 18:35:43 -0300

    Aula 10
    Exercícios + atividade

---
+++ CursoJS-Curso em Video/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) { //condição Simples!
+  console.log("você ultrapassou a velocidade permitida, você foi multado!");
+}
+console.log("Dirijá sempre com o sinto de segurança!");
