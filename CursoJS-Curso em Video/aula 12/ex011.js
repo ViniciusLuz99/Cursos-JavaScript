00c0c33b63c4c142b37d5d7f10988f1c0440ac24
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-04-12 20:47:32 -0300

    Fim da Aula 12 de Javascript do Cursom em Video

---
+++ CursoJS-Curso em Video/aula 12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 70;
+if (idade < 16) {
+  console.log("Não vota");
+} else if (idade < 18 || idade > 68) {
+  console.log("voto opcional");
+} else {
+  console.log("Voto obrigatorio");
+}
