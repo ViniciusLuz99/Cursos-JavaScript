bbd5fff8f8dde6c5b2a6c22be95d0c0ed3929b27
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-05-25 20:58:42 -0300

    FCC

---
+++ Js-FreeCodeCamp/Basic Javascript/37-usar-notao-de-colchetes-para-descobrir-o-ensimo-caractere-antes-do-ltimo-em-uma-string.js
@@ -0,0 +1,17 @@
+// Configuração
+const lastName = "Lovelace";
+
+// Altere apenas o código abaixo desta linha
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Altere esta linha
+/*}
+Usar notação de colchetes para descobrir o enésimo caractere antes do último em uma string
+Você pode usar o mesmo princípio que nós acabamos de usar para recuperar o último caractere em uma string, para recuperar o enésimo caractere antes do último caractere.
+
+Por exemplo, você pode pegar o valor da antepenúltima letra da string const firstName = "Augusta" usando firstName[firstName.length - 3]
+
+Exemplo:
+
+const firstName = "Augusta";
+const thirdToLastLetter = firstName[firstName.length - 3];
+thirdToLastLetter teria o valor da string s.
+ */
---
+++ Js-FreeCodeCamp/Basic Javascript/38-preencher-espaos-em-branco.js
@@ -0,0 +1,21 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Altere apenas o código abaixo desta linha
+var wordBlanks =
+  "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Altere esta linha
+// Altere apenas o código acima desta linha
+
+/*
+Preencher espaços em branco
+Você recebe frases com algumas palavras faltando, como substantivos, verbos, adjetivos e advérbios. Você então preencherá os pedaços faltantes com palavras de sua escolha de modo que a frase completa faça sentido.
+
+Considere esta frase:
+
+It was really ____, and we ____ ourselves ____.
+Essa frase possui três pedaços faltando - um adjetivo, um verbo e um advérbio, e nós podemos adicionar palavras de nossa escolha para completar. Em seguida, podemos atribuir a frase completa para uma variável como se segue:
+
+const sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + "."; 
+*/
