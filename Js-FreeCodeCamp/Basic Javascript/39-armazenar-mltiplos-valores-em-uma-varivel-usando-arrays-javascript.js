e83a26fadeb95d5bface52a4bfc5474c3104c89d
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-05-25 21:06:41 -0300

    exercicios do FCC js

---
+++ Js-FreeCodeCamp/Basic Javascript/39-armazenar-mltiplos-valores-em-uma-varivel-usando-arrays-javascript.js
@@ -0,0 +1,10 @@
+// Altere apenas o código abaixo desta linha
+const myArray = ["teste", 1];
+//armazenar-mltiplos-valores-em-uma-varivel-usando-arrays-javascript
+/*
+Armazenar múltiplos valores em uma variável usando arrays JavaScript
+Com as variáveis de array em JavaScript, podemos armazenar diversos dados em um único lugar.
+
+Você começa uma declaração de um array com a abertura de um colchetes, terminando com o fechamento do colchetes e colocando vírgulas entre cada entrada, dessa forma:
+
+const sandwich = ["peanut butter", "jelly", "bread"]; */
---
+++ Js-FreeCodeCamp/Basic Javascript/40-aninhar-um-array-em-outro-array.js
@@ -0,0 +1,12 @@
+// Altere apenas o código abaixo desta linha
+const myArray = [
+  ["teste", 1],
+  ["teste", 2],
+];
+
+/*
+Aninhar um array em outro array
+Você também pode aninhar arrays dentro de outros arrays, como abaixo:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+Isso é chamado um array multidimensional.*/
