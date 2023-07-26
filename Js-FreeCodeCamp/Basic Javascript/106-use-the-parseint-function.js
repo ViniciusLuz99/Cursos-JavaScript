4b71fadaa8eba3fa928d93a8acd72edcc465e75d
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-07-26 13:33:21 -0300

    Create 106-use-the-parseint-function.js

---
+++ Js-FreeCodeCamp/Basic Javascript/106-use-the-parseint-function.js
@@ -0,0 +1,12 @@
+// Função para converter uma string em um valor inteiro (número inteiro).
+// Recebe uma string 'str' como argumento a ser convertida em um número inteiro.
+function convertToInteger(str) {
+  // A função parseInt() é usada para converter a string em um valor inteiro (número inteiro).
+  // A variável 'converter' armazena o resultado da conversão.
+  let converter = parseInt(str);
+
+  // Retorna o valor inteiro resultante da conversão.
+  return converter;
+}
+
+convertToInteger("56");
