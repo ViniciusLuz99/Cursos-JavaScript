6c836ea5591a497a24fbfbf5ee7d32822d0ee026
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-07-26 13:07:11 -0300

    Create 105-generate-random-fractions-with-javascript.js

---
+++ Js-FreeCodeCamp/Basic Javascript/105-generate-random-fractions-with-javascript.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+  // Altere apenas o código abaixo desta linha
+  let result = 0
+  while (result === 0){
+    result = Math.random();
+  }
+  return result;
+
+  // Altere apenas o código acima desta linha
+}
