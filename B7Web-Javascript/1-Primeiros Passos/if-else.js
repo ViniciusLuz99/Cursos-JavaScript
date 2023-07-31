82441b1beec6fceee22f26a1d4f1e58e52a1ab80
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-07-31 20:28:55 -0300

    Create if-else.js

---
+++ B7Web-Javascript/1-Primeiros Passos/if-else.js
@@ -0,0 +1,14 @@
+let idade = 14;
+
+if (idade >= 18) {
+  console.log("Você é maior de idade.")
+} else {
+  console.log("Você é menor de idade")
+}
+
+// <  menopr
+// >  maior
+// == igual
+// >= maior ou igual
+// <= menor ou igual
+//!= diferente
