ee5eb049a34b033d4c16cacbe21b978515fc29a7
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-01 20:26:03 -0300

    Create Exercício-de-condicional2.js

---
+++ B7Web-Javascript/1-Primeiros Passos/Exercício-de-condicional2.js
@@ -0,0 +1,23 @@
+//resultado seja TRUE
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+//resultado seja FALSE
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+//resultado seja TRUE
+let a = 10;
+let b = 5;
+console.log(a != b);
+
+// verificar o preço da carne esta barato ou caro
+let preco = 40.3;
+
+if (preco <= 45) {
+  console.log("Barato");
+} else {
+  console.log("Caro");
+}
