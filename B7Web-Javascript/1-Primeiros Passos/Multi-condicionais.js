f1ed5929c75ec728a9e15dc284eb325f5a2cee74
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-01 20:05:01 -0300

    Create Multi-condicionais.js

---
+++ B7Web-Javascript/1-Primeiros Passos/Multi-condicionais.js
@@ -0,0 +1,17 @@
+//Multi-condicionais (&& e ||)
+
+let idade = 90
+
+// if (idade >= 18) {
+//   if (idade < 60) {
+//     console.log("Voce é um adulto")
+//   }
+// }
+
+// && é como se fosse um "e, i"
+// || é como se fosse um "ou"
+
+if (idade >= 18 && idade < 60) {
+    console.log("Voce é um adulto")
+
+}
