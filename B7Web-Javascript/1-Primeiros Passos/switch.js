10bd293049caf74bdd69b72b0e6ad6ef1d6302da
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-02 11:17:40 -0300

    Create switch.js

---
+++ B7Web-Javascript/1-Primeiros Passos/switch.js
@@ -0,0 +1,17 @@
+let profession = "fiscal";
+console.log("Profissão: " + profession);
+
+switch (profession) {
+  case "fiscal":
+    console.log("Sua camisa sera verde");
+    break;
+  case "bombeiro":
+    console.log("Sua camisa sera vermelha");
+    break;
+  case "policial":
+    console.log("Sua camisa sera azul");
+    break;
+  default:
+    console.log("Sua camisa sera preta");
+    break;
+}
