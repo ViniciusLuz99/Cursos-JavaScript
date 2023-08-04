7dc12163c2e697e891422776dfa24edf02d88333
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-04 11:37:23 -0300

    Aula 5 modulo 2 B7

--- B7Web-Javascript/2-Javascript Basico/1-Funções/funcao.js
+++ B7Web-Javascript/2-Javascript Basico/1-Funções/funcao.js
@@ -1,3 +1,4 @@
+//Criar a função
 function gravidade() {
   console.log("A gravidade do planeta é:");
   console.log(9.8);
@@ -11,11 +12,14 @@ function somar(numero1, numero2) {
   let result = numero1 + numero2;
   console.log("Resultado: " + result);
 }
-// 10 vai ser o numero1, e 50 o numero2
-somar(10, 50);
+somar(10, 50); // 10 vai ser o numero1, e 50 o numero2
 
+//Retorno da função
+//Entrada -> Processamento -> Saida
 function nomeCompleto(nome, sobrenome) {
-  console.log(`${nome} ${sobrenome}`);
+  return `${nome} ${sobrenome}`; // returno
 }
-nomeCompleto("Bonieky", "Lacertda");
-nomeCompleto("Joao", "Silva");
+
+nomeCompleto("Bonieky", "Lacertda"); //não esta sendo armazenada
+let completo = nomeCompleto("Bonieky", "Lacertda"); //agora sim esta sendo armazenada e tem um valor sendo retornado
+console.log(completo)
