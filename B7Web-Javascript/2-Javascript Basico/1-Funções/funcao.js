3e29484a77e0778ed5002a77976ea35a6f71208b
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-04 11:27:03 -0300

    Aula 4 modulo 2 B7

--- B7Web-Javascript/2-Javascript Basico/1-Funções/funcao.js
+++ B7Web-Javascript/2-Javascript Basico/1-Funções/funcao.js
@@ -1,7 +1,21 @@
 function gravidade() {
-  console.log('A gravidade do planeta é:')
-  console.log(9.8)
+  console.log("A gravidade do planeta é:");
+  console.log(9.8);
 }
 
 //Como usar a função?
 gravidade(); //chamda da função
+
+//Passando parâmetros em funções
+function somar(numero1, numero2) {
+  let result = numero1 + numero2;
+  console.log("Resultado: " + result);
+}
+// 10 vai ser o numero1, e 50 o numero2
+somar(10, 50);
+
+function nomeCompleto(nome, sobrenome) {
+  console.log(`${nome} ${sobrenome}`);
+}
+nomeCompleto("Bonieky", "Lacertda");
+nomeCompleto("Joao", "Silva");
