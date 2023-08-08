40b071e93e2abc688ff188d4ac0420b15b05c01e
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-08 20:08:31 -0300

    Create index.js

---
+++ B7Web-Javascript/2-Javascript Basico/Exercicios/5-Loops/index.js
@@ -0,0 +1,10 @@
+let fruits = ["Maçã", "uva", "banana"];
+for (let f = 0; f < fruits.length; f++) {
+  console.log(fruits[f]);
+}
+
+let numero = 1;
+while (numero <= 100) {
+  console.log(n);
+  numero++;
+}
