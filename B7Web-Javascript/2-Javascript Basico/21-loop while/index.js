37bea9420058aa4ccce2fca80149ef5abb50f9e6
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-08 19:59:46 -0300

    Aula 21 Modulo 2 b7

---
+++ B7Web-Javascript/2-Javascript Basico/21-loop while/index.js
@@ -0,0 +1,15 @@
+// while = enquanto
+let numero = 0;
+while (numero <= 10) {
+  console.log(`O numero é ${numero}`);
+  numero++;
+}
+///igual mas com for
+for (let numeros = 0; numeros <= 10; numeros++) {
+  console.log(`O numero é ${numeros}`);
+}
+
+//estrutura
+while (condicinal) {
+  //
+}
