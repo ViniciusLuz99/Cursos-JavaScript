99f6192ad32432d04040bab71f3144a5d3c0c3b2
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-09 10:29:24 -0300

    Aula 23 Modulo 2 b7

---
+++ B7Web-Javascript/2-Javascript Basico/23-Func de arrays/index.js
@@ -0,0 +1,10 @@
+let fruits = ["Maça","Uva","Laranja","Banana"]
+fruits.push('Kiwi')
+
+
+console.log(fruits.length) //tamanho do array, output sera 5
+
+fruits.join(' ')//join n altera o array,ele pega e salva o resultado em uma string
+
+console.log(fruits.join(', '))//Maça, uva, laranja, banana, kiwi
+
