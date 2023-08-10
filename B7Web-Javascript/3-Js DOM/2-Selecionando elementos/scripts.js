374dbfb089a3a940347b085efbb340fe8711b92a
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-08-10 10:18:07 -0300

    .

---
+++ B7Web-Javascript/3-Js DOM/2-Selecionando elementos/scripts.js
@@ -0,0 +1,8 @@
+document
+//um objeto que da acesso a todoas as coisas relacionadas ao documento
+//responsavel pelo conteudo deste documento
+document.getElementsByTagName("h1")
+
+window
+// windows(janela), relacionado a janela inteira
+
